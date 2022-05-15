0a8fbeda9fe2d3435e911db3cfcb4cb3b52044c5
sisithajayawardhane <sisithajayawardhane@gmail.com>
2022-05-15 18:23:27 +0530

    Initial Commit

---
+++ TwitterClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by mac on 2022-05-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
